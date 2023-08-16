096847af9390296c8c0c1b67ec633e8a77dde0e2
sujan khatri <sujansince2059@gmail.com>
2023-08-16 08:37:36 +0545

    created component for navbar and home

---
+++ src/NavBar.jsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return (
+        <nav className="navabr">
+            <h1>Sujan Blogs</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Create new Blog</a>
+            </div>
+        </nav>
+      );
+}
+ 
+export default NavBar;
